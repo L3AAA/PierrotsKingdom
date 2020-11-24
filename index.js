dc4556fb21ccefc78f2d170863eff5c84840891c
L3AAA <67789071+L3AAA@users.noreply.github.com>
2020-11-24 16:36:33 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+setTimeout(function(){
+    setTimeout(function(){
+      $("#loading").css("display","none");
+      $("#box").css("display","none");
+      $("#Gen").removeClass("animated fadeIn");
+      $("#about").removeClass("animated fadeOut");
+      $("#services").removeClass("animated fadeOut");
+      $("#portfolio").removeClass("animated fadeOut");
+       $("#portfolio2").removeClass("animated fadeOut");
+    },1500);
+},1500);
